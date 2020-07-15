f618d191501daaf08e018a61b61d84f5a75890d0
lifeofdan_el <anomfuemedaniel@gmail.com>
2020-07-15 17:44:17 +0100

    Stage 1 solved

---
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -0,0 +1,10 @@
+package bot
+
+fun main() {
+
+    var botName : String = "DanBot"
+    var birthYear : Int = 2020
+    
+    println("Hello! My name is $botName.")
+    println("I was created in $birthYear.")
+}
